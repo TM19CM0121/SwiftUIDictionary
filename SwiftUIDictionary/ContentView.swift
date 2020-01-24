820b445caff922f03cf018805866772e96854246
TM19CM0121 <lbxmasa@icloud.com>
2020-01-24 14:06:39 +0900

    Initial Commit

---
+++ SwiftUIDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDictionary
+//
+//  Created by 高良昌辰 on 2020/01/24.
+//  Copyright © 2020 高良昌辰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
